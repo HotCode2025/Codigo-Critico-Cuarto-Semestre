d9f201493fa623e7f45b011d3ca10a42135734fb
Ariel Mazara <arielmazara@gmail.com>
2026-08-18 23:31:44 -0300

    12.5 Hacemos las pruebas del método -> buscarEstudiantePorId() Video 8. Ref #5

--- Java/Clase 01 Sistema Estudiantes Parte 1/SistemaEstudiantes/src/main/java/UTN/datos/EstudianteDAO.java
+++ Java/Clase 01 Sistema Estudiantes Parte 1/SistemaEstudiantes/src/main/java/UTN/datos/EstudianteDAO.java
@@ -85,5 +85,16 @@ public class EstudianteDAO {
         System.out.println("Listado de Estudiantes: ");
         List<Estudiante> estudiantes = estudianteDao.listarEstudiantes();
         estudiantes.forEach(System.out::println); // Funcion LAMBDA para IMPRIMIR
+
+        // Buscar por id
+        var estudiante1 = new Estudiante(1);
+        System.out.println("Estudiantes antes de la busqueda: "+estudiante1);
+        var encontrado = estudianteDao.buscarEstudiantePorId(estudiante1);
+        if(encontrado)
+            System.out.println("Estudiante encontrado: "+estudiante1);
+        else
+            System.out.println("No se encontro el estudiante: "+estudiante1.getIdEstudiante());
+
+
     }
 }
