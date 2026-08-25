14cf7fc932103c0411e38bad428839ae75090eb7
Ariel Mazara <arielmazara@gmail.com>
2026-08-25 17:20:41 -0300

    Se hace punto 2.4 Exponencial negativo y positivo. Ref #14
    Co-authored-by: AgustinaZu <agustinavictoriazuniga@gmail.com>

--- Python/Clase 02 Sistemsa Numericos Parte 1/tipo_float.py
+++ Python/Clase 02 Sistemsa Numericos Parte 1/tipo_float.py
@@ -9,4 +9,14 @@ print(f'a: {a:.2f}')
 
 # Notación exponencial (valores positivos o negativos)
 a = 3e5
-print(f'a: {a}') #al numero 3 le agregamos 5 ceros
+print(f'a: {a:.2f}') #al numero 3 le agregamos 5 ceros y 2f son dos ceros despues de la coma
+
+#Valores NEGATIVOS
+a = 3e-5
+print(f'a: {a:.5f}')
+
+# Cualquier calculo que incluye un float, todo cambia a float
+
+a = 4.0 + 5
+print(a)
+print(type(a))
