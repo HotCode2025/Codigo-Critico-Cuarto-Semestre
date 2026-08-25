d6f43934b163d137c02dcdaba6e33456bae9ee6e
Ariel Mazara <arielmazara@gmail.com>
2026-08-25 16:45:38 -0300

    Se hace punto 2.1 Comenzamos con -> Decimal, Binario, Octal y Hexadecimal. Ref #14
    Co-authored-by: AgustinaZu <agustinavictoriazuniga@gmail.com>

---
+++ Python/Clase 02 Sistemsa Numericos Parte 1/Sistema_numeracion.py
@@ -0,0 +1,18 @@
+# Profundizando en los sistemas de numeración
+
+#Sistema decimal
+a = 10
+print(f"a decimal: {a}")
+
+#Sistema Binario
+a = 0b1010
+print(f"a binario: {a}")
+
+#Sistema Octal
+a = 0o12
+print(f"a octal: {a}")
+
+#Sistema Hexadecimal
+a = 0xA
+print(f"a hexadecimal: {a}")
+
