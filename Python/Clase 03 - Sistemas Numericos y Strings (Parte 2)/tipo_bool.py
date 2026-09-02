f7b74c88d07102d82eecff737e78c2388e7706e1
Ariel Mazara <arielmazara@gmail.com>
2026-09-02 15:58:55 -0300

    feat: agrega Parte 1 constructor bool (#24)
    Co-authored-by: AgustinaZu <agustinavictoriazuniga@gmail.com>

---
+++ Python/Clase 03 - Sistemas Numericos y Strings (Parte 2)/tipo_bool.py
@@ -0,0 +1,10 @@
+
+# Bool continene los valores True y False
+# Los tipos numericos, es false para el 0 (cero), true para los demas valores
+valor = 0.0
+resultado = bool(valor)
+print(f'valor; {valor}, Resultado: {resultado}')
+
+valor = 0.1
+resultado = bool(valor)
+print(f'valor; {valor}, Resultado: {resultado}')
