8125848f89c93d30292a2188b28b0a428eff5d67
Ariel Mazara <arielmazara@gmail.com>
2026-09-14 19:13:18 -0300

    feat: Completa Clase 05 - Aplicación de Sistema Estudiantes
    Se implementa el menú interactivo con el bloque switch.
    Se agregan las operaciones CRUD: listar, buscar, agregar, modificar y eliminar.
    Se incluye la corrección de formato por consola y el bloque try-catch para excepciones.
    
    Closes #48
    
    Co-authored-by: AgustinaZu <agustinavictoriazuniga@gmail.com>

--- Java/Clase 02 03 y 04 - Aplicacion de Sistema Estudiantes/estudiantes/src/main/java/utn/estudiantes/modelo/Estudiante.java
+++ Java/Clase 02 03 y 04 - Aplicacion de Sistema Estudiantes/estudiantes/src/main/java/utn/estudiantes/modelo/Estudiante.java
@@ -1,15 +1,13 @@
 package utn.estudiantes.modelo;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 @Entity
+@Table(name = "estudiantes2026")
 // boilerplate - Repetitivo código
 @Data
 @NoArgsConstructor
@@ -18,7 +16,7 @@ import lombok.ToString;
 public class Estudiante {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer idEstudiante;
+    private Integer idestudiantes2026;
     private String nombre;
     private String apellido;
     private String telefono;
